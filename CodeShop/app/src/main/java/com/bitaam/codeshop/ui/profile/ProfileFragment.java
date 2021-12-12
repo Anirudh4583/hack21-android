0347040a9a064e9a324de0f67b31e781d3b7db3b
TechMangalam <201951089@iiitvadodara.ac.in>
2021-12-12 11:29:59 +0530

    Navigation Created

---
+++ CodeShop/app/src/main/java/com/bitaam/codeshop/ui/profile/ProfileFragment.java
@@ -0,0 +1,32 @@
+package com.bitaam.codeshop.ui.profile;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bitaam.codeshop.R;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        // Inflate the layout for this fragment
+        return view;
+    }
+}
