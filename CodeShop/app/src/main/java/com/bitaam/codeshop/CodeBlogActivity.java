7a309058e1ad98e48281ec83525ca40ee9b9b624
TechMangalam <201951089@iiitvadodara.ac.in>
2021-12-12 14:19:05 +0530

    added code copy and launcher icon

--- CodeShop/app/src/main/java/com/bitaam/codeshop/CodeBlogActivity.java
+++ CodeShop/app/src/main/java/com/bitaam/codeshop/CodeBlogActivity.java
@@ -2,12 +2,17 @@ package com.bitaam.codeshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bitaam.codeshop.modals.CodeItemModal;
 import com.squareup.picasso.Picasso;
@@ -15,7 +20,7 @@ import com.squareup.picasso.Picasso;
 public class CodeBlogActivity extends AppCompatActivity {
 
     ImageView codeTemplateIv;
-    TextView templateTitle,templateAuthorTv,dateTv,codeTv;
+    TextView templateTitle,templateAuthorTv,dateTv,codeTv,copyCodeTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +35,7 @@ public class CodeBlogActivity extends AppCompatActivity {
         templateAuthorTv = findViewById(R.id.authorNameTv);
         dateTv = findViewById(R.id.templateCreationDateTv);
         codeTv = findViewById(R.id.codeTv);
+        copyCodeTv = findViewById(R.id.copyCodeTv);
 
         if (codeItemModal != null){
             Picasso.get().load(Uri.parse(codeItemModal.getImgUrl())).into(codeTemplateIv);
@@ -39,6 +45,23 @@ public class CodeBlogActivity extends AppCompatActivity {
             codeTv.setText(codeItemModal.getCode());
         }
 
+        copyCodeTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (codeItemModal!=null){
+                    setClipboard(getApplicationContext(),codeItemModal.getCode());
+                    Toast.makeText(getApplicationContext(), "Code copied to clipboard", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
     }
+
+    private void setClipboard(Context context, String text) {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("Copied code snippets", text);
+        clipboard.setPrimaryClip(clip);
+    }
+
 }
